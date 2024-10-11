7c0559f89dbb38ebd9a3701cc98f9296843fa512
Johann Dallmann <JDallmann@conet.de>
2024-10-11 12:10:06 +0200

    Initial Commit

---
+++ src/main/java/de/jd/usermanagementtemplate/UsermanagementtemplateApplication.java
@@ -0,0 +1,13 @@
+package de.jd.usermanagementtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementtemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementtemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/jd/usermanagementtemplate/UsermanagementtemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jd.usermanagementtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementtemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
